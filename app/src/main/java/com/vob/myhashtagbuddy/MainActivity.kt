86184455ad71911307853084868ab1b6b7ee702a
aditya <mishraaditya595@gmail.com>
2021-04-03 13:59:54 +0530

    Added dependencies and made animated bg

--- app/src/main/java/com/vob/myhashtagbuddy/MainActivity.kt
+++ app/src/main/java/com/vob/myhashtagbuddy/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.vob.myhashtagbuddy
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vob.myhashtagbuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var animationDrawable: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        animationDrawable = binding.main.background as AnimationDrawable
+        animationDrawable.setExitFadeDuration(400)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        animationDrawable.start()
     }
 }
