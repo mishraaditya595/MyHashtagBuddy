a9a7dd5f79d2a1f3c7ce43667a2660f67f8c0999
aditya <mishraaditya595@gmail.com>
2021-04-03 19:37:06 +0530

    improvements patch 1

--- app/src/main/java/com/vob/myhashtagbuddy/MainActivity.kt
+++ app/src/main/java/com/vob/myhashtagbuddy/MainActivity.kt
@@ -1,47 +1,62 @@
 package com.vob.myhashtagbuddy
 
 import android.app.Activity
+import android.content.ClipData
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.content.ClipboardManager
+import android.view.View
 import android.widget.Toast
+import androidx.core.view.isEmpty
 import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
-//import com.google.firebase.ml.vision.FirebaseVision
-//import com.google.firebase.ml.vision.common.FirebaseVisionImage
-//import com.google.firebase.ml.vision.text.FirebaseVisionText
+
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabeling
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.vob.myhashtagbuddy.databinding.ActivityMainBinding
 import com.vob.myhashtagbuddy.util.Constants.IMAGE_PICK_CODE
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var animationDrawable: AnimationDrawable
-    private var isText = false
+    private lateinit var copyText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        copyText = "";
 
         animationDrawable = binding.main.background as AnimationDrawable
         animationDrawable.setExitFadeDuration(400)
 
-        binding.mainSwitch.setOnClickListener {
-            isText = binding.mainSwitch.isChecked
-        }
 
         binding.mainFAB.setOnClickListener {
             pickImage()
         }
 
+        if (!binding.mainChipGroup.isEmpty())
+        {
+            binding.copyButton.visibility = View.VISIBLE
+        }
+
+        binding.copyButton.setOnClickListener {
+            if (copyText.isNotBlank() && copyText.isNotEmpty())
+            {
+                val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clipData = ClipData.newPlainText("label", copyText)
+                clipboardManager.setPrimaryClip(clipData)
+                Snackbar.make(binding.root,"Copied to clipboard", Snackbar.LENGTH_SHORT).show()
+                copyText = ""
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -54,10 +69,6 @@ class MainActivity : AppCompatActivity() {
                    val bitmap = getImageFromData(data)
                    bitmap.apply {
                        binding.mainIV.setImageBitmap(this)
-//                       if (!isText)
-//                           bitmap?.let { processImageTagging(it) }
-//                       else
-//                           bitmap?.let { startTextRecognisation(it) }
                        this?.let { labelImage(it) }
                    }
                }
@@ -81,36 +92,24 @@ class MainActivity : AppCompatActivity() {
                 }
                     .forEach {
                         binding.mainChipGroup.addView(it)
+                        binding.copyButton.visibility = View.VISIBLE
+                        copyText += "#"+it.text+" "
                     }
+
+                if (binding.mainChipGroup.isEmpty())
+                {
+                    binding.copyButton.visibility = View.GONE
+                    Snackbar.make(binding.root,"No hashtags could be generated",Snackbar.LENGTH_SHORT).show()
+                }
             }
             .addOnFailureListener { exception ->
                 exception.message?.let {
                     Snackbar.make(binding.root, it, Snackbar.LENGTH_LONG).show()
                 }
+                binding.copyButton.visibility = View.GONE
             }
     }
 
-//    private fun processImageTagging(bitmap: Bitmap) {
-//        val visionImage = FirebaseVisionImage.fromBitmap(bitmap)
-//        FirebaseVision.getInstance().onDeviceImageLabeler.processImage(visionImage)
-//            .addOnSuccessListener { labelList ->
-//                binding.mainChipGroup.removeAllViews()
-//                labelList.sortedByDescending { firebaseVisionImageLabel ->
-//                    firebaseVisionImageLabel.confidence
-//                }.map { visionImageLabel ->
-//                    Chip(this, null, R.style.Widget_MaterialComponents_Chip_Choice)
-//                        .apply { text = visionImageLabel.text }
-//                }
-//                    .forEach {
-//                        binding.mainChipGroup.addView(it)
-//                    }
-//            }
-//            .addOnFailureListener { exception ->
-//                exception.message?.let {
-//                    Snackbar.make(binding.root, it, Snackbar.LENGTH_LONG).show()
-//                }
-//            }
-//    }
 
     private fun getImageFromData(data: Intent?): Bitmap? {
         val selectedImage = data?.data
@@ -120,35 +119,7 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-//    private fun startTextRecognisation(bitmap: Bitmap) {
-//        if (binding.mainIV.drawable != null) {
-//            val image = FirebaseVisionImage.fromBitmap(bitmap)
-//            val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
-//            detector.processImage(image)
-//                .addOnSuccessListener { firbaseVisionText ->
-//                    processTextBlock(firbaseVisionText)
-//                }
-//                .addOnFailureListener {
-//                    it.message?.let { it1 ->
-//                        Snackbar.make(binding.root, it1, Snackbar.LENGTH_LONG).show()
-//                    }
-//                }
-//        }
-//        else
-//        {
-//            Toast.makeText(this, "Failed", Toast.LENGTH_LONG).show()
-//        }
-//    }
-//
-//    private fun processTextBlock(firbaseVisionText: FirebaseVisionText?) {
-//        binding.mainChipGroup.removeAllViews()
-//        firbaseVisionText?.textBlocks?.map {
-//            Chip(this, null, R.style.Widget_MaterialComponents_Chip_Choice)
-//                .apply { text = it.text }
-//        }?.forEach {
-//            binding.mainChipGroup.addView(it)
-//        }
-//    }
+
 
     private fun pickImage() {
         val intent = Intent().apply {
